2677bd0882fd6a360d6854282c97f35ef04831e6
Petar Enev <enev@engineer.bg>
2022-10-25 19:38:42 +0300

    db and alembic improvements

---
+++ app/db/crud.py
@@ -0,0 +1,36 @@
+from app import models
+from app import schemas
+from sqlalchemy.orm import Session
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserInCreate):
+    user.password = user.password + "notreallyhashed"
+    db_user = models.User(**user.dict())
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_animals(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Animal).offset(skip).limit(limit).all()
+
+
+def create_user_animal(db: Session, animal: schemas.Animal, user_id: int):
+    db_animal = models.Animal(**animal.dict(), owner_id=user_id)
+    db.add(db_animal)
+    db.commit()
+    db.refresh(db_animal)
+    return db_animal
--- app/db/database.py
+++ app/db/database.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import sessionmaker
 
 from decouple import config
 
-DATABASE_URL = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@localhost:5433/pets"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@{config('DB_HOST')}:5432/findapet"
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
