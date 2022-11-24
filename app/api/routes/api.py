161c5a70e8558c12e82e2f74470578d4bdf14fd6
Petar Enev <enev@engineer.bg>
2022-11-24 19:09:22 +0200

    minor changes

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -4,5 +4,5 @@ from app.api.routes import animals, auth, users
 
 router = APIRouter()
 router.include_router(animals.router, tags=["animals"])
-router.include_router(auth.router)
+router.include_router(auth.router, tags=["auth"])
 router.include_router(users.router, tags=["users"])
--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,9 @@
+from decouple import config
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from decouple import config
-
-DATABASE_URL = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@{config('DB_HOST')}:5432/findapet"
+DATABASE_URL = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@{config('DB_HOST')}:{config('DB_PORT')}/findapet"
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
